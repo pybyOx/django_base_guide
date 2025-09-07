7ff77acbe36a37bbe9426ca478728e579b631890
Oksana Ushakova <oksanchik123456@mail.ru>
2025-09-07 20:05:32 +0700

    make db models and add in admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['category', 'name', 'price', 'available', 'created', 'updated']
+    list_filter = ['category', 'available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name', )}
